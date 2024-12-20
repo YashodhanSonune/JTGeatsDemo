534305afd080b7a883eb2b59d7a630fd38c090eb
Yashodhan Sonune <152440746+YashodhanSonune@users.noreply.github.com>
2024-12-20 21:07:20 +0530

    almost done

--- emptycarttoggle.js
+++ emptycarttoggle.js
@@ -2,5 +2,5 @@ const cartButton = document.getElementById("shopping");
 const cartDiv = document.getElementById("empty_cart_container");
 
 cartButton.addEventListener("click", () => {
-  cartDiv.style.zIndex = "10";
+  cartDiv.style.transform = "translateX(150%)";
 });
